12e39e8130bf3c4c7b68b7b226ab6360a5b49909
Hannes <Hannes@127.0.0.1>
2015-04-24 15:41:00 +0200

    Testing

---
+++ src/testing/johannesTest/CSVReader.java
@@ -0,0 +1,14 @@
+package testing.johannesTest;
+
+import java.io.FileReader;
+import java.util.List;
+
+import com.opencsv.*;
+
+public class CSVReader {
+	CSVReader reader;
+
+	public void importFile(String path) {
+		
+	}
+}
