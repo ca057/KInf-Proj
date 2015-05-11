4642b7b410925a2196a8af1426feed019babcb89
simps90 <simon.stemper@gmail.com>
2015-05-11 12:46:08 +0200

    Added access to internal database

--- src/testing/simonsTest/TestDB.java
+++ src/testing/simonsTest/TestDB.java
@@ -2,7 +2,6 @@ package testing.simonsTest;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -14,21 +13,18 @@ import java.sql.Statement;
  */
 public class TestDB {
 	void test() {
-		String dbURL = "jdbc:derby:C:/Users/Simon/MyDB;create=true";
+		String dbURL = "jdbc:derby:C:/Users/Simon/git/KInf-Projekt/db/MyDB;create=true";
 		String user = "test";
 		String password = "test";
 
 		try (Connection con = DriverManager
 				.getConnection(dbURL, user, password);
-				Statement stmt = con.createStatement();
-				ResultSet rs = stmt
-						.executeQuery("SELECT * FROM studenten.person");) {
+				Statement stmt = con.createStatement();) {
 			if (con != null) {
 				System.out.println("Connected to DB");
 			}
-			if (rs != null) {
-				System.out.println(rs);
-			}
+			stmt.execute("CREATE SCHEMA table_test");
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
