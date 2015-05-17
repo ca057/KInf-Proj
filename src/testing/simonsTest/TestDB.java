f4b04ec8c4379935a80b4c8a5a79daf591e31b54
simon.stemper@gmail.com <simon.stemper@gmail.com>
2015-05-17 14:45:14 +0200

    Moved location of database to relative location

--- src/testing/simonsTest/TestDB.java
+++ src/testing/simonsTest/TestDB.java
@@ -13,7 +13,7 @@ import java.sql.Statement;
  */
 public class TestDB {
 	void test() {
-		String dbURL = "jdbc:derby:C:/Users/Simon/git/KInf-Projekt/db/MyDB;create=true";
+		String dbURL = "jdbc:derby:./db/MyDB;create=true";
 		String user = "test";
 		String password = "test";
 
