7a86b5bf0d7a98c5058e2fc9b85cd63ffe138998
ca057 <ca@smo.de>
2015-05-21 09:28:07 +0200

    Added image to alert "Über"

--- src/testing/christiansTest/javaFX/MenuController.java
+++ src/testing/christiansTest/javaFX/MenuController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -68,6 +69,8 @@ public class MenuController {
 		Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle(ui.getAppName() + " - Über");
 		alert.setHeaderText(ui.getAppName() + " v1.82.02.22.3");
+		alert.setGraphic(new ImageView(this.getClass()
+				.getResource("einhorn.gif").toString()));
 		String content = "Mit Liebe gemacht von\nSimon Stemper, Johannes Trepesch, Christian Ost";
 		content += "\n\u00A9 2015 WTFPL – Do What the Fuck You Want to Public License";
 		alert.setContentText(content);
