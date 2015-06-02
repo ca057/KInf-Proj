3d1326c641ee9424631a1afc0325660db27c9864
Christian Ost <ca@smo.de>
2015-06-02 18:25:36 +0200

    Added imports and ids of javaFX-elements to controller.

--- src/de/uniba/kinf/projm/hylleblomst/view/Controller.java
+++ src/de/uniba/kinf/projm/hylleblomst/view/Controller.java
@@ -1,5 +1,75 @@
 package de.uniba.kinf.projm.hylleblomst.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
+
 public class Controller {
 
+	@FXML
+	MenuBar mainMenu;
+
+	@FXML
+	Menu mainMenu_file;
+
+	@FXML
+	MenuItem mainMenu_file_save;
+
+	@FXML
+	MenuItem mainMenu_file_export;
+
+	@FXML
+	MenuItem mainMenu_file_close;
+
+	@FXML
+	Menu mainMenu_edit;
+
+	@FXML
+	MenuItem mainMenu_edit_options;
+
+	@FXML
+	Menu mainMenu_help;
+
+	@FXML
+	MenuItem mainMenu_help_help;
+
+	@FXML
+	MenuItem mainMenu_help_about;
+
+	@FXML
+	TextField search_fulltext;
+
+	@FXML
+	TitledPane searchCategory_person;
+
+	@FXML
+	TitledPane searchCategory_personExtended;
+
+	@FXML
+	TitledPane searchCategory_study;
+
+	@FXML
+	TitledPane searchCategory_other;
+
+	@FXML
+	ButtonBar searchMenu;
+
+	@FXML
+	Button searchMenu_clearSearch;
+
+	@FXML
+	Button searchMenu_search;
+
+	@FXML
+	TableView resultTable;
+
+	@FXML
+	TextArea infoArea;
 }
