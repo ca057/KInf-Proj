482dd57429975f8ffa44351ca6c392d195f9eaba
HannesT117 <j.trepesch@outlook.de>
2015-06-08 13:49:06 +0200

    Changed structure: Added new package for exceptions, data to database happens now in package database.

---
+++ src/de/uniba/kinf/projm/hylleblomst/database/Validation.java
@@ -0,0 +1,17 @@
+package de.uniba.kinf.projm.hylleblomst.database;
+
+public class Validation {
+
+	/**
+	 * Checks if an ID is already in use in the database.
+	 * 
+	 * @return true if taken <br/>
+	 *         false if new ID
+	 */
+	public static boolean isTakenID() {
+		// TODO Implement: If person number is taken, return true. (via
+		// SQL-query)
+		return false;
+	}
+
+}
--- src/de/uniba/kinf/projm/hylleblomst/main/Main.java
+++ src/de/uniba/kinf/projm/hylleblomst/main/Main.java
@@ -1,7 +1,7 @@
 package de.uniba.kinf.projm.hylleblomst.main;
 
 import de.uniba.kinf.projm.hylleblomst.dataImport.Import;
-import de.uniba.kinf.projm.hylleblomst.dataImport.ImportException;
+import de.uniba.kinf.projm.hylleblomst.exceptions.ImportException;
 
 public class Main {
 
