569f755cc2d5d2cd19b6d130f23aa27f6af40dd9
HannesT117 <j.trepesch@outlook.de>
2015-06-10 10:41:06 +0200

    Fixed error in main

--- src/de/uniba/kinf/projm/hylleblomst/main/Main.java
+++ src/de/uniba/kinf/projm/hylleblomst/main/Main.java
@@ -7,7 +7,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-			Import.addImport("/Users/Hannes/git/KInf-Proj/doc/example_data.csv");
+			new Import()
+					.addCSV("/Users/Hannes/git/KInf-Proj/doc/example_data.csv");
 		} catch (ImportException e) {
 			e.printStackTrace();
 		}
