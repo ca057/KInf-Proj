e57663ddc89334c0271e755f378a2102baca33ed
Christian Ost <ca@smo.de>
2015-06-13 13:56:01 +0200

    Changes in Controller.

--- src/de/uniba/kinf/projm/hylleblomst/view/Controller.java
+++ src/de/uniba/kinf/projm/hylleblomst/view/Controller.java
@@ -138,7 +138,7 @@ public class Controller {
 	 * info area.
 	 */
 	private void setInfoText() {
-		String info = "Suchanfragen\n------------";
+		String info = "Suchanfrage\n-----------";
 		info += "Volltextsuche: " + getFullTextSearchInput() + "\n";
 		infoArea.setText(info);
 	}
