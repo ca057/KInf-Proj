d5b0a53ed2152a750dd6473e3f77a6494a165cbe
Christian Ost <ca@smo.de>
2015-06-13 21:16:27 +0200

    Added Fulltext search button to fxml and added method head to controller

--- src/de/uniba/kinf/projm/hylleblomst/main/Main.java
+++ src/de/uniba/kinf/projm/hylleblomst/main/Main.java
@@ -1,14 +1,11 @@
 package de.uniba.kinf.projm.hylleblomst.main;
 
-import de.uniba.kinf.projm.hylleblomst.dataImport.ImportDataImpl;
-import de.uniba.kinf.projm.hylleblomst.exceptions.ImportException;
-
 public class Main {
 
 	public static void main(String[] args) {
 		try {
-			new ImportDataImpl().addData("doc/example_data.csv");
-		} catch (ImportException e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
