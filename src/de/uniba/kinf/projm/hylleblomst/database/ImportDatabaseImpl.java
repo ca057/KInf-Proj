7ff35395249788af905f984b783a788ecb429213
simon.stemper@gmail.com <simon.stemper@gmail.com>
2015-06-13 12:49:25 +0200

    Continued work on inserting logic

--- src/de/uniba/kinf/projm/hylleblomst/database/ImportDatabaseImpl.java
+++ src/de/uniba/kinf/projm/hylleblomst/database/ImportDatabaseImpl.java
@@ -16,6 +16,13 @@ public class ImportDatabaseImpl implements ImportDatabase {
 	private String user;
 	private String password;
 
+	private final int[] quellenID = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+	private final String[] quellen = { "Standard", "HS B (AUB, I 11)",
+			"HS C (AUB, I 13/1)", "HS D (AUB, I 13/2)", "HS E (AUB, I 9)",
+			"HS F (AUB, I 8)", "HS G (AUB, I 6)",
+			"HS H (AEB, Rep. I, Nr. 321)", "HS I (SB Bamberg, Msc.Add.3a)",
+			"HS J (SB Bamberg, Msc.Add.3)", "AUB, V E 38" };
+
 	private String insertSql = "INSERT into ";
 
 	public ImportDatabaseImpl(String dbURL, String user, String password)
@@ -33,52 +40,110 @@ public class ImportDatabaseImpl implements ImportDatabase {
 
 	@Override
 	public void importData(List<String[]> rows) throws ImportException {
-		System.out.println("Kommt in ImportDatabaseImpl an" + rows);
+		System.out.println("Kommt in ImportDatabaseImpl an");
+
+		for (int i = 0; i < quellen.length; i++) {
+			insertIntoTableOneAttributeNoForeignKeys("quellen", quellen[i]);
+		}
 
 		rows = rows.subList(1, rows.size());
 
 		for (String[] strings : rows) {
+			int personID = Integer.parseInt(strings[0]);
+
+			int anredeNormID = 0;
+			int anredeTradID = 0;
+			int fakultaetenID = 0;
+			int fundortID = 0;
+			int titelNormID = 0;
+			int titelTradID = 0;
+
+			int vornameNormID = 0;
+			int fachNormID = 0;
+			int wirtschaftslageNormID = 0;
+			int seminarNormID = 0;
+
+			int vornameTradID;
 
 			if (!strings[4].equals("")) {
-				insertAnredeNorm(strings[4]);
+				anredeNormID = insertIntoTableOneAttributeNoForeignKeys(
+						"anrede_norm", strings[4]);
 			}
-			if (!strings[6].equals("")) {
-				insertVornameNorm(strings[6]);
-			}
-			if (!strings[18].equals("")) {
-				insertNameNorm(strings[18]);
-			}
-			if (!strings[42].equals("")) {
-				insertOrtAbweichungNorm(strings[42]);
+			if (!strings[3].equals("")) {
+				anredeTradID = insertIntoTableOneAttributeOneForeignKey(
+						"anrede_trad", strings[3], anredeNormID);
 			}
 			if (!strings[47].equals("")) {
-				// insertFakultaeten(strings[47]);
-			}
-			if (!strings[49].equals("")) {
-				insertFachNorm(strings[49]);
-			}
-			if (!strings[52].equals("")) {
-				// insertWirtschaftslageNorm(strings[52]);
-			}
-			if (!strings[57].equals("")) {
-				// insertSeminarNorm(strings[57]);
-			}
-			if (!strings[65].equals("")) {
-				// insertTitelNorm(strings[65]);
+				fakultaetenID = insertIntoTableOneAttributeNoForeignKeys(
+						"fakultaeten", strings[47]);
 			}
 			if (!strings[77].equals("")) {
-				// insertFundorte(strings[77]);
+				fundortID = insertIntoTableOneAttributeNoForeignKeys(
+						"fundorte_norm", strings[77]);
 			}
+			if (!strings[65].equals("")) {
+				titelNormID = insertIntoTableOneAttributeNoForeignKeys(
+						"titel_norm", strings[65]);
+			}
+			if (!strings[64].equals("")) {
+				titelTradID = insertIntoTableOneAttributeOneForeignKey(
+						"titel_trad", strings[64], titelNormID);
+			}
+			// TODO Person can now be inserted
+
+			if (!strings[6].equals("")) {
+				vornameNormID = insertIntoTableOneAttributeNoForeignKeys(
+						"vorname_norm", strings[6]);
+			}
+			if (!strings[5].equals("")) {
+				vornameTradID = insertIntoTableOneAttributeOneForeignKey(
+						"vorname_trad", strings[5], vornameNormID);
+				// insertIntoTableNoAttributesThreeForeignKeys("vorname_info",
+				// vornameTradID, quellenID[0], personID);
+			}
+			if (!strings[18].equals("")) {
+				// insertNameIntoSimpleTable("name_norm", strings[47]);
+			}
+			if (!strings[42].equals("")) {
+				// insertOrtAbweichungNorm(strings[42]);
+			}
+
+			if (!strings[49].equals("")) {
+				fachNormID = insertIntoTableOneAttributeNoForeignKeys(
+						"fach_norm", strings[49]);
+			}
+			if (!strings[48].equals("")) {
+				insertIntoTableOneAttributeOneForeignKey("fach_trad",
+						strings[48], fachNormID);
+			}
+			if (!strings[52].equals("")) {
+				wirtschaftslageNormID = insertIntoTableOneAttributeNoForeignKeys(
+						"wirtschaftslage_norm", strings[52]);
+			}
+			if (!strings[51].equals("")) {
+				insertIntoTableOneAttributeOneForeignKey(
+						"wirtschaftslage_trad", strings[51],
+						wirtschaftslageNormID);
+			}
+			if (!strings[57].equals("")) {
+				seminarNormID = insertIntoTableOneAttributeNoForeignKeys(
+						"seminar_norm", strings[57]);
+			}
+			if (!strings[56].equals("")) {
+				insertIntoTableOneAttributeOneForeignKey("seminar_trad",
+						strings[56], seminarNormID);
+			}
+
 		}
 	}
 
-	// TODO Erweiterung um Anmerkungen (Spalte 78)
+	// TODO Erweiterung um Anmerkungen
 	private boolean insertOrtAbweichungNorm(String nameNorm)
 			throws ImportException {
 		String table = "hylleblomst.Ort_Abweichung_Norm";
 		String column = "Name";
 
-		if (!validation.entryAlreadyInDatabase(nameNorm, table, column)) {
+		if (!validation.entryAlreadyInDatabase(nameNorm, table)) {
 			try (Connection con = DriverManager.getConnection(dbURL, user,
 					password); Statement stmt = con.createStatement();) {
 
@@ -93,107 +158,105 @@ public class ImportDatabaseImpl implements ImportDatabase {
 				return true;
 			} catch (SQLException e) {
 				throw new ImportException(
-						"A AbweichungNorm could not be inserted", e);
+						"A AbweichungNorm could not be inserted"
+								+ e.getMessage());
 			}
 		}
 		return false;
 	}
 
-	private boolean insertVornameNorm(String nameNorm) throws ImportException {
-		String table = "hylleblomst.Vorname_Norm";
-		String column = "Name";
+	/**
+	 * Inserts an entry into a specified table of the schema
+	 * <code>HYLLEBLOMST</code>. This method is very simple and is only suited
+	 * for tables which do not have more attributes than an id and one varchar.
+	 * 
+	 * @param table
+	 *            The table in which to insert
+	 * @param entry
+	 *            The varchar to introduce into table
+	 * @return The unique ID of the entry, whether is was already in the
+	 *         database or not
+	 * @throws ImportException
+	 *             If an error occurred during writing to database
+	 */
+	private int insertIntoTableOneAttributeNoForeignKeys(String table,
+			String entry) throws ImportException {
+		table = String.format("hylleblomst.%s", table);
 
-		if (!validation.entryAlreadyInDatabase(nameNorm, table, column)) {
+		int id;
+
+		if (!validation.entryAlreadyInDatabase(entry, table)) {
 			try (Connection con = DriverManager.getConnection(dbURL, user,
 					password); Statement stmt = con.createStatement();) {
 
-				int id = validation.getMaxID(table) + 1;
+				id = validation.getMaxID(table) + 1;
 
 				String insertStmt = String.format(
 						"%1$s %2$s values(%3$d, '%4$s')", insertSql, table, id,
-						nameNorm);
+						entry);
 
 				stmt.execute(insertStmt);
 
-				return true;
 			} catch (SQLException e) {
-				throw new ImportException(
-						"A VornameNorm could not be inserted", e);
+				throw new ImportException(String.format(
+						"A %s could not be inserted", entry) + e.getMessage());
 			}
+		} else {
+			id = validation.getIDOfEntry(entry, table);
 		}
-		return false;
+		return id;
 	}
 
-	private boolean insertNameNorm(String nameNorm) throws ImportException {
-		String table = "hylleblomst.Name_Norm";
-		String column = "Name";
+	private int insertIntoTableOneAttributeOneForeignKey(String table,
+			String entry, int foreignKey) throws ImportException {
+		if (foreignKey == 0) {
+			throw new ImportException("ForeignKey is empty");
+		}
+		table = String.format("hylleblomst.%s", table);
 
-		if (!validation.entryAlreadyInDatabase(nameNorm, table, column)) {
+		int id;
+
+		if (!validation.entryAlreadyInDatabase(entry, foreignKey, table)) {
 			try (Connection con = DriverManager.getConnection(dbURL, user,
 					password); Statement stmt = con.createStatement();) {
 
-				int id = validation.getMaxID(table) + 1;
+				id = validation.getMaxID(table) + 1;
 
 				String insertStmt = String.format(
-						"%1$s %2$s values(%3$d, '%4$s')", insertSql, table, id,
-						nameNorm);
+						"%1$s %2$s values(%3$d, '%4$s', %5$d)", insertSql,
+						table, id, entry, foreignKey);
 
 				stmt.execute(insertStmt);
 
-				return true;
 			} catch (SQLException e) {
-				throw new ImportException("A NameNorm could not be inserted", e);
+				throw new ImportException(String.format(
+						"A %s could not be inserted", entry) + e.getMessage());
 			}
+		} else {
+			id = validation.getIDOfEntry(entry, foreignKey, table);
 		}
-		return false;
+		return id;
 	}
 
-	private boolean insertFachNorm(String fachNorm) throws ImportException {
-		String table = "hylleblomst.Fach_Norm";
-		String column = "Name";
+	private void insertIntoTableNoAttributesThreeForeignKeys(String table,
+			int tradID, int quellenID, int personID) throws ImportException {
+		table = String.format("hylleblomst.%s", table);
 
-		if (!validation.entryAlreadyInDatabase(fachNorm, table, column)) {
+		if (!validation.entryAlreadyInDatabase(tradID, quellenID, personID,
+				table)) {
 			try (Connection con = DriverManager.getConnection(dbURL, user,
 					password); Statement stmt = con.createStatement();) {
 
-				int id = validation.getMaxID(table) + 1;
-
 				String insertStmt = String.format(
-						"%1$s %2$s values(%3$d, '%4$s')", insertSql, table, id,
-						fachNorm);
+						"%1$s %2$s values(%3$d, %4$d, %5$d)", insertSql, table,
+						tradID, quellenID, personID);
 
 				stmt.execute(insertStmt);
 
-				return true;
 			} catch (SQLException e) {
-				throw new ImportException("A FachNorm could not be inserted", e);
+				throw new ImportException("An info table could not be created"
+						+ e.getMessage());
 			}
 		}
-		return false;
-	}
-
-	private boolean insertAnredeNorm(String anredeNorm) throws ImportException {
-		String table = "hylleblomst.Anrede_Norm";
-		String column = "Name";
-
-		if (!validation.entryAlreadyInDatabase(anredeNorm, table, column)) {
-			try (Connection con = DriverManager.getConnection(dbURL, user,
-					password); Statement stmt = con.createStatement();) {
-
-				int id = validation.getMaxID(table) + 1;
-
-				String insertStmt = String.format(
-						"%1$s %2$s values(%3$d, '%4$s')", insertSql, table, id,
-						anredeNorm);
-
-				stmt.execute(insertStmt);
-
-				return true;
-			} catch (SQLException e) {
-				throw new ImportException("A AnredeNorm could not be inserted",
-						e);
-			}
-		}
-		return false;
 	}
 }
--- src/de/uniba/kinf/projm/hylleblomst/database/Validation.java
+++ src/de/uniba/kinf/projm/hylleblomst/database/Validation.java
@@ -3,6 +3,7 @@ package de.uniba.kinf.projm.hylleblomst.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -45,8 +46,10 @@ public class Validation {
 	 * @return <code>True</code> if entry was found, <code>false</code>
 	 *         otherwise
 	 */
-	boolean entryAlreadyInDatabase(String entry, String table, String column) {
+	boolean entryAlreadyInDatabase(String entry, String table) {
 		boolean result = true;
+
+		String column = getColumnName(table, 2);
 
 		try (Connection con = DriverManager
 				.getConnection(dbURL, user, password);
@@ -60,10 +63,10 @@ public class Validation {
 			ResultSet rs = stmt.executeQuery(querySql);
 
 			con.setAutoCommit(false);
+
 			/*
 			 * Returns true if ResultSet is not empty, false otherwise
 			 */
-
 			result = rs.isBeforeFirst();
 
 			con.setAutoCommit(true);
@@ -76,15 +79,115 @@ public class Validation {
 		return result;
 	}
 
+	boolean entryAlreadyInDatabase(String entry, int foreignKey, String table) {
+		boolean result = true;
+
+		String columnTwo = getColumnName(table, 2);
+		String columnThree = getColumnName(table, 3);
+
+		try (Connection con = DriverManager
+				.getConnection(dbURL, user, password);
+				Statement stmt = con.createStatement(
+						ResultSet.TYPE_SCROLL_INSENSITIVE,
+						ResultSet.CONCUR_READ_ONLY)) {
+
+			String querySql = String
+					.format("SELECT %2$s, %3$s FROM %1$s WHERE %2$s='%4$s' AND %3$s=%5$d",
+							table, columnTwo, columnThree, entry, foreignKey);
+			ResultSet rs = stmt.executeQuery(querySql);
+
+			con.setAutoCommit(false);
+
+			/*
+			 * Returns true if ResultSet is not empty, false otherwise
+			 */
+			result = rs.isBeforeFirst();
+
+			con.setAutoCommit(true);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	boolean entryAlreadyInDatabase(int tradID, int quellenID, int personID,
+			String table) {
+		boolean result = true;
+
+		String columnOne = getColumnName(table, 1);
+		String columnTwo = getColumnName(table, 2);
+		String columnThree = getColumnName(table, 3);
+
+		try (Connection con = DriverManager
+				.getConnection(dbURL, user, password);
+				Statement stmt = con.createStatement(
+						ResultSet.TYPE_SCROLL_INSENSITIVE,
+						ResultSet.CONCUR_READ_ONLY)) {
+
+			String querySql = String
+					.format("SELECT %2$s, %3$s, %4$s FROM %1$s WHERE %2$s=%5$d AND %3$s=%6$d AND %4$s=%7$d",
+							table, columnOne, columnTwo, columnThree, tradID,
+							quellenID, personID);
+			ResultSet rs = stmt.executeQuery(querySql);
+
+			con.setAutoCommit(false);
+
+			/*
+			 * Returns true if ResultSet is not empty, false otherwise
+			 */
+			result = rs.isBeforeFirst();
+
+			con.setAutoCommit(true);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	/**
+	 * Retrieves the name of a specified column in a table
+	 * 
+	 * @param table
+	 *            The table in which the column exists
+	 * @param i
+	 *            The index of the column
+	 * @return The name of the column as {@link String}
+	 */
+	private String getColumnName(String table, int i) {
+		String result = "";
+
+		try (Connection con = DriverManager
+				.getConnection(dbURL, user, password);
+				Statement stmt = con.createStatement(
+						ResultSet.TYPE_SCROLL_INSENSITIVE,
+						ResultSet.CONCUR_READ_ONLY)) {
+
+			String querySql = String.format("SELECT * FROM %s", table);
+			ResultSet rs = stmt.executeQuery(querySql);
+			ResultSetMetaData rsmd = rs.getMetaData();
+
+			result = rsmd.getColumnName(i);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
 	/**
 	 * Returns the highest ID already taken in a table of the schema HYLLEBLOMST
 	 * 
-	 * @param tableName
-	 *            The table in which to look for highest given ID Must be in the
+	 * @param table
+	 *            The table in which to look for highest given ID must be in the
 	 *            format <code>HYLLEBLOMST.table_name</code>
 	 * @return The maximum ID as <code>int</code>
 	 */
-	int getMaxID(String tableName) {
+	int getMaxID(String table) {
 		int result = 0;
 
 		try (Connection con = DriverManager
@@ -93,10 +196,10 @@ public class Validation {
 						ResultSet.TYPE_SCROLL_INSENSITIVE,
 						ResultSet.CONCUR_READ_ONLY)) {
 
-			String columnName = (tableName.replace("_", "") + "ID").replace(
+			String column = (table.replace("_", "") + "ID").replace(
 					"hylleblomst.", "");
 			String getMaxIDSQL = String.format("SELECT max(%s) FROM %s",
-					columnName, tableName);
+					column, table);
 			ResultSet rs = stmt.executeQuery(getMaxIDSQL);
 
 			con.setAutoCommit(false);
@@ -111,5 +214,76 @@ public class Validation {
 		}
 
 		return result;
+	}
+
+	/**
+	 * Gets the ID of a pre-existent entry in a specified table
+	 * 
+	 * @param entry
+	 *            The entry which's ID is search
+	 * @param table
+	 *            The table in which to look for the ID
+	 * @return The ID of the searched entry if existent in specified table,
+	 *         <code>0</code> otherwise
+	 */
+	int getIDOfEntry(String entry, String table) {
+		int id = 0;
+
+		try (Connection con = DriverManager
+				.getConnection(dbURL, user, password);
+				Statement stmt = con.createStatement(
+						ResultSet.TYPE_SCROLL_INSENSITIVE,
+						ResultSet.CONCUR_READ_ONLY)) {
+
+			String column = getColumnName(table, 2);
+			String querySql = String.format(
+					"SELECT * FROM %2$s WHERE %1$s='%3$s'", column, table,
+					entry);
+			ResultSet rs = stmt.executeQuery(querySql);
+
+			con.setAutoCommit(false);
+
+			rs.next();
+			id = rs.getInt(1);
+
+			con.setAutoCommit(true);
+
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return id;
+	}
+
+	int getIDOfEntry(String entry, int foreignKey, String table) {
+		// TODO Auto-generated method stub
+		int id = 0;
+
+		String columnTwo = getColumnName(table, 2);
+		String columnThree = getColumnName(table, 3);
+
+		try (Connection con = DriverManager
+				.getConnection(dbURL, user, password);
+				Statement stmt = con.createStatement(
+						ResultSet.TYPE_SCROLL_INSENSITIVE,
+						ResultSet.CONCUR_READ_ONLY)) {
+
+			String querySql = String.format(
+					"SELECT * FROM %1$s WHERE %2$s='%4$s' AND %3$s=%5$d",
+					table, columnTwo, columnThree, entry, foreignKey);
+			ResultSet rs = stmt.executeQuery(querySql);
+
+			con.setAutoCommit(false);
+
+			rs.next();
+			id = rs.getInt(1);
+
+			con.setAutoCommit(true);
+
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return id;
 	}
 }
