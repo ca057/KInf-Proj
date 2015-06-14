db5f21609db2898562e78622cac37d2985b7978d
HannesT117 <j.trepesch@outlook.de>
2015-06-14 03:27:58 +0200

    no idea.

--- src/de/uniba/kinf/projm/hylleblomst/logic/SourceKeys.java
+++ src/de/uniba/kinf/projm/hylleblomst/logic/SourceKeys.java
@@ -2,17 +2,17 @@ package de.uniba.kinf.projm.hylleblomst.logic;
 
 public class SourceKeys {
 
-	public static Integer NORM = 0;
-	public static Integer STANDARD = 1;
-	public static Integer HSB_AUB_I11 = 2;
-	public static Integer HSC_AUB_I131 = 3;
-	public static Integer HSD_AUB_I132 = 4;
-	public static Integer HSE_AUB_I9 = 5;
-	public static Integer HSF_AUB_I8 = 6;
-	public static Integer HSG_AUB_I6 = 7;
-	public static Integer HSH_AEB_I321 = 8;
-	public static Integer HSI_SB_3a = 9;
-	public static Integer HSJ_3 = 10;
-	public static Integer AUB_V_E38 = 11;
+	public static final Integer NORM = 0;
+	public static final Integer STANDARD = 1;
+	public static final Integer HSB_AUB_I11 = 2;
+	public static final Integer HSC_AUB_I131 = 3;
+	public static final Integer HSD_AUB_I132 = 4;
+	public static final Integer HSE_AUB_I9 = 5;
+	public static final Integer HSF_AUB_I8 = 6;
+	public static final Integer HSG_AUB_I6 = 7;
+	public static final Integer HSH_AEB_I321 = 8;
+	public static final Integer HSI_SB_3a = 9;
+	public static final Integer HSJ_3 = 10;
+	public static final Integer AUB_V_E38 = 11;
 
 }
