0d7296689014ddd24f586fd7785a30eae28bd619
HannesT117 <j.trepesch@outlook.de>
2015-06-15 17:03:23 +0200

    Added switch-cases for all enums to give the QueryRequest the right table and column.

--- src/de/uniba/kinf/projm/hylleblomst/logic/QueriesImpl.java
+++ src/de/uniba/kinf/projm/hylleblomst/logic/QueriesImpl.java
@@ -7,9 +7,11 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Collection;
+import java.util.HashSet;
 
 public class QueriesImpl implements Queries {
 	DBAccess db;
+	public HashSet<String> set = new HashSet<String>();
 
 	@Override
 	public void search(Collection<QueryRequest> queryRequests) {
@@ -21,18 +23,11 @@ public class QueriesImpl implements Queries {
 		// In WHERE auch SELECT möglich!!
 		StringBuffer query = new StringBuffer();
 
-		// TODO Spaltennamen herausfinden
 		String dbURL = "jdbc:derby:Users/Hannes/git/KInf-Proj/db/MyDB";
 		String user = "admin";
 		String password = "password";
 		db = new DBAccess(dbURL, user, password);
-		// db = new DBAccess();
-		// try {
-		// db.startQuery("SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'Tabellenname'");
-		// } catch (SQLException e) {
-		// TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
+
 	}
 
 	public String getColumnName(String table) {
@@ -40,7 +35,6 @@ public class QueriesImpl implements Queries {
 		String user = "admin";
 		String password = "password";
 		String result = "";
-
 		try (Connection con = DriverManager
 				.getConnection("jdbc:derby:/Users/Hannes/git/KInf-Proj/db/MyDB; user=admin; password=password");
 				Statement stmt = con.createStatement(
@@ -51,132 +45,23 @@ public class QueriesImpl implements Queries {
 			ResultSet rs = stmt.executeQuery(querySql);
 			ResultSetMetaData rsmd = rs.getMetaData();
 			int size = rsmd.getColumnCount();
-			for (int i = 0; i < size; i++) {
+
+			for (int i = 1; i <= size; i++) {
 				result += rsmd.getColumnName(i) + "\n";
+				set.add(rsmd.getTableName(i));
+
 			}
+			System.out.println(rsmd.getTableName(1));
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+
 		return result;
 	}
 
 	@Override
 	public void setDatabase(String dbURL, String user, String password) {
 		db = new DBAccess(dbURL, user, password);
-	}
-
-	/**
-	 * Returns the name of the table the {@code SearchFieldKey} key belongs to.
-	 * 
-	 * @param key
-	 * @return
-	 */
-	// TODO Collection/ List durchgehen, PreparedStatement mit so vielen ?
-	// (person.vorname=?) wie Queries, dann nochmal durchgehen und fragezeichen
-	// füllen. WICHTIG: Collection muss sortiert sein!
-	private String searchFieldKeyToTable(SearchFieldKeys key) {
-		// TODO In QueryRequest moven
-		if (key.toString().equals("ADELIG")) {
-			return "PERSON";
-		} else if (key.toString().equals("JESUIT")) {
-			return "PERSON";
-		} else if (key.toString().equals("STUDIENJAHR")) {
-			return "PERSON";
-		} else if (key.toString().equals("STUDJAHR_VON")) {
-			return "PERSON";
-		} else if (key.toString().equals("STUDJAHR_BIS")) {
-			return "PERSON";
-		} else if (key.toString().equals("EINSCHREIBEDATUM_TAGE")) {
-			return "PERSON";
-		} else if (key.toString().equals("EINSCHREIBEDATUM_MONATE")) {
-			return "PERSON";
-		} else if (key.toString().equals("EINSCHREIBEDATUM_JAHRE")) {
-			return "PERSON";
-		} else if (key.toString().equals("ANMERKUNGEN")) {
-			return "PERSON";
-		} else if (key.toString().equals("NUMMER")) {
-			return "PERSON";
-		} else if (key.toString().equals("SEITE_ORIGINALE")) {
-			return "PERSON";
-		} else if (key.toString().equals("NUMMER_HESS")) {
-			return "PERSON";
-		} else {
-			return key.name();
-		}
-	}
-
-	private String searchFieldKeyToColumn(SearchFieldKeys key) {
-		// TODO In QueryRequest moven
-		if (key.toString().equals("ADELIG")) {
-			return "PERSON";
-		} else if (key.toString().equals("JESUIT")) {
-			return "PERSON";
-		} else if (key.toString().equals("STUDIENJAHR")) {
-			return "PERSON";
-		} else if (key.toString().equals("STUDJAHR_VON")) {
-			return "PERSON";
-		} else if (key.toString().equals("STUDJAHR_BIS")) {
-			return "PERSON";
-		} else if (key.toString().equals("EINSCHREIBEDATUM_TAGE")) {
-			return "PERSON";
-		} else if (key.toString().equals("EINSCHREIBEDATUM_MONATE")) {
-			return "PERSON";
-		} else if (key.toString().equals("EINSCHREIBEDATUM_JAHRE")) {
-			return "PERSON";
-		} else if (key.toString().equals("ANMERKUNGEN")) {
-			return "PERSON";
-		} else if (key.toString().equals("NUMMER")) {
-			return "PERSON";
-		} else if (key.toString().equals("SEITE_ORIGINALE")) {
-			return "PERSON";
-		} else if (key.toString().equals("NUMMER_HESS")) {
-			return "PERSON";
-		} else if (key.toString().equals("ANREDE_TRAD")) {
-			return "";
-		} else if (key.toString().equals("ANREDE_NORM")) {
-			return "";
-		} else if (key.toString().equals("TITEL_TRAD")) {
-			return "";
-		} else if (key.toString().equals("TITEL_NORM")) {
-			return "";
-		} else if (key.toString().equals("VORNAME_NORM")) {
-			return "";
-		} else if (key.toString().equals("NACHNAME_TRAD")) {
-			return "";
-		} else if (key.toString().equals("NACHNAME_NORM")) {
-			return "";
-		} else if (key.toString().equals("WIRTSCHAFTSLAGE_TRAD")) {
-			return "";
-		} else if (key.toString().equals("WIRTSCHAFTSLAGE_NORM")) {
-			return "";
-		} else if (key.toString().equals("ORT_TRAD")) {
-			return "";
-		} else if (key.toString().equals("ORT_NORM")) {
-			return "";
-		} else if (key.toString().equals("ORT_ABWEICHUNG_NORM")) {
-			return "";
-		} else if (key.toString().equals("FACH_TRAD")) {
-			return "";
-		} else if (key.toString().equals("FACH_NORM")) {
-			return "";
-		} else if (key.toString().equals("FAKULTAETEN")) {
-			return "";
-		} else if (key.toString().equals("SEMINAR_TRAD")) {
-			return "";
-		} else if (key.toString().equals("SEMINAR_NORM")) {
-			return "";
-		} else if (key.toString().equals("GRADUIERT")) {
-			return "";
-		} else if (key.toString().equals("ZUSAETZE")) {
-			return "";
-		} else if (key.toString().equals("FUNDORTE")) {
-			return "PERSON";
-		} else {
-			throw new IllegalArgumentException(
-					"Das zugehörige Tabellenelement für Suchfeld " + key.name()
-							+ " ist nicht definiert.");
-		}
-
 	}
 
 }
--- src/de/uniba/kinf/projm/hylleblomst/logic/QueryRequest.java
+++ src/de/uniba/kinf/projm/hylleblomst/logic/QueryRequest.java
@@ -4,6 +4,8 @@ public class QueryRequest {
 	private SearchFieldKeys searchField;
 	private Object input;
 	private int source;
+	private String table;
+	private String column;
 
 	public QueryRequest(SearchFieldKeys columns, Object input, int source) {
 		setSearchField(columns);
@@ -36,5 +38,267 @@ public class QueryRequest {
 
 	public void setSource(int source) {
 		this.source = source;
+	}
+
+	/**
+	 * Returns the name of the table the {@code SearchFieldKey} key belongs to.
+	 * 
+	 * @param key
+	 * @return
+	 */
+	// TODO Collection/ List durchgehen, PreparedStatement mit so vielen ?
+	// (person.vorname=?) wie Queries, dann nochmal durchgehen und fragezeichen
+	// füllen. WICHTIG: Collection muss sortiert sein!
+
+	private void searchFieldKeyToDatabaseData(SearchFieldKeys key) {
+		switch (key) {
+		case ADELIG:
+			table = "PERSON";
+			column = "ADLIG";
+			break;
+		case JESUIT:
+			table = "PERSON";
+			column = "JESUIT";
+			break;
+		case STUDIENJAHR:
+			table = "PERSON";
+			column = "STUDIENJAHR";
+			break;
+		case STUDJAHR_VON:
+			// TODO bei von bis wird es ein Problem geben, oder?
+			table = "PERSON";
+			column = "STUDIENJAHR";
+			break;
+		case STUDJAHR_BIS:
+			// TODO bei von bis wird es ein Problem geben, oder?
+			table = "PERSON";
+			column = "STUDIENJAHR";
+			break;
+		case EINSCHREIBEDATUM_TAGE:
+			// TODO Das muss am besten vorher in ein Datum umgewandelt werden.
+			// Oder übergibt Christian hier eh keine Einzeldaten?
+			table = "PERSON";
+			column = "";
+			break;
+		case EINSCHREIBEDATUM_MONATE:
+			table = "PERSON";
+			column = "";
+			break;
+		case EINSCHREIBEDATUM_JAHRE:
+			table = "PERSON";
+			column = "";
+			break;
+		case ANMERKUNGEN:
+			table = "PERSON";
+			column = "ANMKERUNG";
+			break;
+		case NUMMER:
+			table = "PERSON";
+			column = "PERSONID";
+			break;
+		case SEITE_ORIGINALE:
+			table = "PERSON";
+			column = "SEITEORIGINAL";
+			break;
+		case NUMMER_HESS:
+			table = "PERSON";
+			column = "NUMMERHESS";
+			break;
+		case ANREDE_TRAD:
+			table = "ANREDE_TRAD";
+			column = "NAME";
+			break;
+		case ANREDE_NORM:
+			table = "ANREDE_NORM";
+			column = "NAME";
+			break;
+		case TITEL_TRAD:
+			table = "TITEL_TRAD";
+			column = "TITEL";
+			break;
+		case TITEL_NORM:
+			table = "TITEL_NORM";
+			column = "NAME";
+			break;
+		case VORNAME_TRAD:
+			table = "VORNAME_TRAD";
+			column = "NAME";
+			break;
+		case VORNAME_NORM:
+			table = "VORNAME_NORM";
+			column = "NAME";
+			break;
+		case NACHNAME_TRAD:
+			table = "NAME_TRAD";
+			column = "NAMETRAD";
+			break;
+		case NACHNAME_NORM:
+			table = "NAME_TRAD";
+			column = "NAMETRAD";
+			break;
+		case WIRTSCHAFTSLAGE_TRAD:
+			table = "WIRTSCHAFTSLAGE_TRAD";
+			column = "NAME";
+			break;
+		case WIRTSCHAFTSLAGE_NORM:
+			table = "WIRTSCHAFTSLAGE_NORM";
+			column = "NAME";
+			break;
+		case ORT_TRAD:
+			table = "ORT_TRAD";
+			column = "SCHREIBWEISE";
+			break;
+		case ORT_NORM:
+			table = "ORT_NORM";
+			column = "ORTNORM";
+			break;
+		case ORT_ABWEICHUNG_NORM:
+			// TODO Hier muss dann auch die Anmerkung mit zurückgegeben werden.
+			table = "ORT_ABWEICHUNG_NORM";
+			column = "NAME";
+			break;
+		case FACH_TRAD:
+			table = "FACH_TRAD";
+			column = "NAME";
+			break;
+		case FACH_NORM:
+			table = "FACH_NORM";
+			column = "NAME";
+			break;
+		case FAKULTAETEN:
+			table = "FAKULTAETEN";
+			column = "NAME";
+			break;
+		case SEMINAR_TRAD:
+			table = "SEMINAR_TRAD";
+			column = "SCHREIBWEISE";
+			break;
+		case SEMINAR_NORM:
+			table = "SEMINAR_NORM";
+			column = "NAME";
+			break;
+		case GRADUIERT:
+			table = "PERSON";
+			column = "GRADUIERT";
+			break;
+		case ZUSAETZE:
+			table = "ZUSAETZE";
+			column = "INHALT";
+			break;
+		case FUNDORTE:
+			table = "FUNDORTE";
+			column = "NAME";
+			break;
+		default:
+			throw new IllegalArgumentException(
+					"Das zugehörige Tabellenelement für Suchfeld " + key.name()
+							+ " ist nicht definiert.");
+		}
+		// if (key.toString().equals("ADELIG")) {
+		// table = "PERSON";
+		// column = "ADLIG";
+		// } else if (key.toString().equals("JESUIT")) {
+		// table = "PERSON";
+		// column = "JESUIT";
+		// } else if (key.toString().equals("STUDIENJAHR")) {
+		// table = "PERSON";
+		// column = "STUDIENJAHR";
+		// // TODO bei von bis wird es ein Problem geben, oder?
+		// } else if (key.toString().equals("STUDJAHR_VON")) {
+		// table = "PERSON";
+		// column = "STUDIENJAHR";
+		// } else if (key.toString().equals("STUDJAHR_BIS")) {
+		// table = "PERSON";
+		// column = "STUDIENJAHR";
+		// } else if (key.toString().equals("EINSCHREIBEDATUM_TAGE")) {
+		// table = "PERSON";
+		// column = "";
+		// } else if (key.toString().equals("EINSCHREIBEDATUM_MONATE")) {
+		// table = "PERSON";
+		// column = "";
+		// } else if (key.toString().equals("EINSCHREIBEDATUM_JAHRE")) {
+		// table = "PERSON";
+		// column = "";
+		// } else if (key.toString().equals("ANMERKUNGEN")) {
+		// table = "PERSON";
+		// column = "ANMKERUNG";
+		// } else if (key.toString().equals("NUMMER")) {
+		// table = "PERSON";
+		// column = "PERSONID";
+		// } else if (key.toString().equals("SEITE_ORIGINALE")) {
+		// table = "PERSON";
+		// column = "SEITEORIGINAL";
+		// } else if (key.toString().equals("NUMMER_HESS")) {
+		// table = "PERSON";
+		// column = "NUMMERHESS";
+		// } else if (key.toString().equals("ANREDE_TRAD")) {
+		// table = "ANREDE_TRAD";
+		// column = "NAME";
+		// } else if (key.toString().equals("ANREDE_NORM")) {
+		// table = "ANREDE_NORM";
+		// column = "NAME";
+		// } else if (key.toString().equals("TITEL_TRAD")) {
+		// table = "TITEL_TRAD";
+		// column = "TITEL";
+		// } else if (key.toString().equals("TITEL_NORM")) {
+		// table = "TITEL_NORM";
+		// column = "NAME";
+		// } else if (key.toString().equals("VORNAME_TRAD")) {
+		// table = "VORNAME_TRAD";
+		// column = "NAME";
+		// } else if (key.toString().equals("VORNAME_NORM")) {
+		// table = "VORNAME_NORM";
+		// column = "NAME";
+		// } else if (key.toString().equals("NACHNAME_TRAD")) {
+		// table = "NAME_TRAD";
+		// column = "NAMETRAD";
+		// } else if (key.toString().equals("NACHNAME_NORM")) {
+		// table = "NAME_TRAD";
+		// column = "NAMETRAD";
+		// } else if (key.toString().equals("WIRTSCHAFTSLAGE_TRAD")) {
+		// table = "WIRTSCHAFTSLAGE_TRAD";
+		// column = "NAME";
+		// } else if (key.toString().equals("WIRTSCHAFTSLAGE_NORM")) {
+		// table = "WIRTSCHAFTSLAGE_NORM";
+		// column = "NAME";
+		// } else if (key.toString().equals("ORT_TRAD")) {
+		// table = "ORT_TRAD";
+		// column = "SCHREIBWEISE";
+		// } else if (key.toString().equals("ORT_NORM")) {
+		// table = "ORT_NORM";
+		// column = "ORTNORM";
+		// } else if (key.toString().equals("ORT_ABWEICHUNG_NORM")) {
+		// // TODO Hier muss dann auch die Anmerkung mit zurückgegeben werden.
+		// table = "ORT_ABWEICHUNG_NORM";
+		// column = "NAME";
+		// } else if (key.toString().equals("FACH_TRAD")) {
+		// table = "FACH_TRAD";
+		// column = "NAME";
+		// } else if (key.toString().equals("FACH_NORM")) {
+		// table = "FACH_NORM";
+		// column = "NAME";
+		// } else if (key.toString().equals("FAKULTAETEN")) {
+		// table = "FAKULTAETEN";
+		// column = "NAME";
+		// } else if (key.toString().equals("SEMINAR_TRAD")) {
+		// table = "SEMINAR_TRAD";
+		// column = "SCHREIBWEISE";
+		// } else if (key.toString().equals("SEMINAR_NORM")) {
+		// table = "SEMINAR_NORM";
+		// column = "NAME";
+		// } else if (key.toString().equals("GRADUIERT")) {
+		// table = "PERSON";
+		// column = "GRADUIERT";
+		// } else if (key.toString().equals("ZUSAETZE")) {
+		// table = "ZUSAETZE";
+		// column = "INHALT";
+		// } else if (key.toString().equals("FUNDORTE")) {
+		// table = "FUNDORTE";
+		// column = "NAME";
+		// } else {
+		// throw new IllegalArgumentException(
+		// "Das zugehörige Tabellenelement für Suchfeld " + key.name()
+		// + " ist nicht definiert.");
+		// }
 	}
 }
