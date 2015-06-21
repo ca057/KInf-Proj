8a4e469a853a886e6426570a7f5dd2af0feb3024
HannesT117 <j.trepesch@outlook.de>
2015-06-21 11:48:19 +0200

    Added SQL for Ort_Abweichung_Norm

--- src/de/uniba/kinf/projm/hylleblomst/logic/SQLShred.java
+++ src/de/uniba/kinf/projm/hylleblomst/logic/SQLShred.java
@@ -5,11 +5,46 @@ public class SQLShred {
 	String getPersonJoin(String table, String column, int source) {
 
 		String tableName = table.substring(0, table.indexOf("_"));
-		if (source == SourceKeys.ORT_NORM_AB) {
-			// TODO implement this
-			System.out
-					.println("Join-Version für ORT_NORM_AB noch nicht implementiert.");
-			return null;
+		if (table.startsWith("Ort")) {
+			return "Hylleblomst."
+					+ tableName
+					+ "_abweichung_norm, Hylleblomst."
+					+ tableName
+					+ "_norm, Hylleblomst."
+					+ tableName
+					+ "_trad, Hylleblomst."
+					+ tableName
+					+ "_info, Hylleblomst.quellen, Hylleblomst.person "
+					+ "WHERE Hylleblomst."
+					+ tableName
+					+ "_trad."
+					+ tableName
+					+ "TradID = Hylleblomst."
+					+ tableName
+					+ "_info."
+					+ tableName
+					+ "TradID AND Hylleblomst."
+					+ tableName
+					+ "_norm."
+					+ tableName
+					+ "NormID = Hylleblomst."
+					+ tableName
+					+ "_trad."
+					+ tableName
+					+ "NormID AND Hylleblomst."
+					+ tableName
+					+ "_abweichung_norm."
+					+ tableName
+					+ "AbweichungNormID = Hylleblomst."
+					+ tableName
+					+ "_norm."
+					+ tableName
+					+ "NormID AND Hylleblomst."
+					+ tableName
+					+ "_info.QuellenID = Hylleblomst.quellen.QuellenID AND Hylleblomst."
+					+ tableName
+					+ "_info.PersonID = Hylleblomst.person.PersonID AND Hylleblomst."
+					+ tableName + "_info.QuellenID = " + source;
 		}
 		if (table.startsWith("ANREDE")) {
 			return "Hylleblomst." + tableName + "_norm, Hylleblomst."
