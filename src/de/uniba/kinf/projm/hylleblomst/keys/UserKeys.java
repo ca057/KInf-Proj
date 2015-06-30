2a5b78ce393e14ac96040b8d0e845c78da4be9a9
simon.stemper@gmail.com <simon.stemper@gmail.com>
2015-06-30 13:43:27 +0200

    Added UserKeys which contains usernames and password. Maybe not the most secure way to handle password, but handy for the moment.

---
+++ src/de/uniba/kinf/projm/hylleblomst/keys/UserKeys.java
@@ -0,0 +1,11 @@
+package de.uniba.kinf.projm.hylleblomst.keys;
+
+public class UserKeys {
+	public final static String dbURL = "jdbc:derby:./db/MyDB";
+
+	public final static String guestUser = "guest";
+	public final static String guestPassword = "ned-stark";
+
+	public final static String adminUser = "admin";
+	public final static String adminPassword = "r+l=j";
+}
