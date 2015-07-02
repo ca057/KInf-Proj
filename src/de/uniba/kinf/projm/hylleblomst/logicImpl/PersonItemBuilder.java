84b7cba8e8295e24735ad595655c82c434cbaf32
Christian Ost <ca@smo.de>
2015-07-02 13:33:41 +0200

    Changed layout.

--- src/de/uniba/kinf/projm/hylleblomst/logicImpl/PersonItemBuilder.java
+++ src/de/uniba/kinf/projm/hylleblomst/logicImpl/PersonItemBuilder.java
@@ -1,6 +1,9 @@
 package de.uniba.kinf.projm.hylleblomst.logicImpl;
 
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 
@@ -26,7 +29,20 @@ public class PersonItemBuilder {
 			throw new InputMismatchException(
 					"The given ResultSet has no value (is null).");
 		}
-		//
-		return null;
+		// analyze the given ResultSet
+		try {
+			ResultSetMetaData metaData = resultSet.getMetaData();
+			int numberOfColumns = metaData.getColumnCount();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// build the PersonItem
+		List<PersonItem> personItemsList = new ArrayList<PersonItem>();
+		// add it to a list
+
+		// return a list
+		return personItemsList;
 	}
 }
--- src/de/uniba/kinf/projm/hylleblomst/view/MainGUI.java
+++ src/de/uniba/kinf/projm/hylleblomst/view/MainGUI.java
@@ -1,10 +1,12 @@
 package de.uniba.kinf.projm.hylleblomst.view;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class MainGUI extends Application {
 	private UIHelper ui = new UIHelper();
@@ -20,12 +22,14 @@ public class MainGUI extends Application {
 			primaryStage.setTitle(ui.getAppName());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			/*
-			 * primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-			 * public void handle(WindowEvent we) { if
-			 * (ui.askForClosingWindow(primaryStage)) { primaryStage.close(); }
-			 * else { we.consume(); } } });
-			 */
+
+			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+				public void handle(WindowEvent we) {
+					primaryStage.close();
+					// TODO check if correct
+					System.exit(0);
+				}
+			});
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
