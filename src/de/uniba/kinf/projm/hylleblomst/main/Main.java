73f7eb85966c25e9a9a88732595218c68e58e157
HannesT117 <j.trepesch@outlook.de>
2015-07-08 11:03:26 +0200

    Updated main

--- src/de/uniba/kinf/projm/hylleblomst/main/Main.java
+++ src/de/uniba/kinf/projm/hylleblomst/main/Main.java
@@ -8,6 +8,10 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	public static void main(String[] args) {
+		launch(args);
+	}
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// Logik starten
