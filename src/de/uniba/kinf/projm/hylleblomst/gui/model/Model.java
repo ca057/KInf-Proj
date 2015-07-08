f61beb847260a49ec6ae2c35ce06b7fcb02a026b
HannesT117 <j.trepesch@outlook.de>
2015-07-08 11:34:09 +0200

    Tried to structure everything a little bit better.

--- src/de/uniba/kinf/projm/hylleblomst/gui/model/Model.java
+++ src/de/uniba/kinf/projm/hylleblomst/gui/model/Model.java
@@ -2,25 +2,41 @@ package de.uniba.kinf.projm.hylleblomst.gui.model;
 
 import java.sql.SQLException;
 import java.util.Collection;
-import java.util.List;
+import java.util.InputMismatchException;
 import java.util.Observable;
 
-import de.uniba.kinf.projm.hylleblomst.logic.PersonItem;
+import javax.sql.rowset.CachedRowSet;
+
 import de.uniba.kinf.projm.hylleblomst.logic.SearchInitiator;
 import de.uniba.kinf.projm.hylleblomst.logic.UserQueries;
 
 public class Model extends Observable {
+	SearchInitiator search;
+	CachedRowSet searchResult;
 
-	public Model(SearchInitiator initiator) {
-
+	public Model(SearchInitiator search) {
+		if (search != null) {
+			this.search = search;
+		} else {
+			throw new InputMismatchException("Die Logik des Programms ist fehlerhaft: " + search);
+		}
 	}
 
 	public void search(Collection<UserQueries> userQueries) throws SQLException {
-
+		if (!(userQueries == null || userQueries.isEmpty())) {
+			searchResult = search.search(userQueries);
+			notifyObservers();
+		} else {
+			throw new InputMismatchException("Die übergebene Collection ist fehlerhaft: " + search);
+		}
 	}
 
-	public List<PersonItem> searchPerson(String id) throws SQLException {
-		return null;
+	public void searchPerson(String id) throws SQLException {
+		if (id != null) {
+			searchResult = search.searchPerson(id);
+			notifyObservers();
+		} else {
+			throw new InputMismatchException("Die übergebene ID ist fehlerhaft: " + search);
+		}
 	}
-
 }
