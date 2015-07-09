f69213dbb8e85fe6a5fbc4881d7e2db44d642d67
simon.stemper@gmail.com <simon.stemper@gmail.com>
2015-07-09 13:28:56 +0200

    Added a format verifier for data import, implementation under way

---
+++ src/de/uniba/kinf/projm/hylleblomst/dataImport/CsvFormatVerifier.java
@@ -0,0 +1,16 @@
+package de.uniba.kinf.projm.hylleblomst.dataImport;
+
+import java.io.File;
+
+public class CsvFormatVerifier {
+
+	public File csvFile;
+
+	public CsvFormatVerifier(File file) {
+		this.csvFile = file;
+	}
+
+	public boolean verifyCsv() {
+		return false;
+	}
+}
