3c4ce7285f527347a4cc00e48d91b3fda1b64a1b
HannesT117 <j.trepesch@outlook.de>
2015-07-10 09:39:26 +0200

    Removed commented code, renamed variable

--- src/de/uniba/kinf/projm/hylleblomst/logicImpl/DBAccess.java
+++ src/de/uniba/kinf/projm/hylleblomst/logicImpl/DBAccess.java
@@ -40,16 +40,15 @@ public class DBAccess {
 	}
 
 	/**
-	 * @param query
+	 * @param sqlStatement
 	 * @param inputs
 	 * @return
 	 * @throws SQLException
 	 */
-	public CachedRowSet startQuery(String query, List<String> inputs) throws SQLException {
+	public CachedRowSet startQuery(String sqlStatement, List<String> inputs) throws SQLException {
 		CachedRowSet crs = new CachedRowSetImpl();
-		// CachedRowSet crs = new RowSetFactoryImpl().createCachedRowSet();
 		try (Connection con = DriverManager.getConnection(dbURL, user, password);
-				PreparedStatement stmt = con.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE,
+				PreparedStatement stmt = con.prepareStatement(sqlStatement, ResultSet.TYPE_SCROLL_INSENSITIVE,
 						ResultSet.CONCUR_READ_ONLY);) {
 			int parameterIndex = 1;
 			if (inputs != null) {
@@ -62,32 +61,6 @@ public class DBAccess {
 			con.setAutoCommit(false);
 			crs.populate(stmt.executeQuery());
 			con.setAutoCommit(true);
-
-			// crs.getArray(ColumnNameKeys.DATUM);
-			// crs.getArray(ColumnNameKeys.DATUMS_FELDER_GESETZT);
-			//
-			// ResultSetMetaData rsmd = stmt.getMetaData();
-			// int columnsNumber = rsmd.getColumnCount();
-			// while (results.next()) {
-			// StringBuilder string2 = new StringBuilder();
-			// for (int x = 0; x < columnsNumber; x++) {
-			// string2.append(results.getString((x + 1)) + ", ");
-			// }
-			// System.out.println(string2.substring(0, string2.length() -
-			// 2).toString());
-			// }
-			// for (int i = 1; i < results.getFetchSize(); i++) {
-			// results.getString(i);
-			// String blub = results.getString("DATUM_INT");
-			// String date = results.getString(ColumnNameKeys.DATUM);
-			// if ("010".equals(blub)) {
-			// date = date.substring(0, 4) + "X" + date.substring(7, 9);
-			// } else if ("001".equals(blub)) {
-			// date = date.substring(0, 7) + "X";
-			// } else if ("011".equals(blub)) {
-			// date = date.substring(0, 4) + "X.X";
-			// }
-			// }
 
 		}
 		return crs;
