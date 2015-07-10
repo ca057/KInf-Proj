8b24ce8db606b29a5e13c8bca51b9bbd2ff5da64
simon.stemper@gmail.com <simon.stemper@gmail.com>
2015-07-10 16:32:05 +0200

    Implemented groupConcat

--- src/de/uniba/kinf/projm/hylleblomst/database/sql/utils/GroupConcat.java
+++ src/de/uniba/kinf/projm/hylleblomst/database/sql/utils/GroupConcat.java
@@ -1,8 +1,26 @@
 package de.uniba.kinf.projm.hylleblomst.database.sql.utils;
 
+/**
+ * @author Simon
+ *
+ */
 public class GroupConcat {
+	/**
+	 * This function returns a concatenation of the given Strings separated by
+	 * the given separator
+	 * 
+	 * @param separator
+	 * @param args
+	 * @return A String in the format "arg0, arg1, ..., argN"
+	 */
+	public String groupConcat(String separator, String... args) {
+		StringBuilder result = new StringBuilder("");
 
-	public String groupConcat() {
-		return "";
+		for (String arg : args) {
+			result.append(arg + separator + " ");
+		}
+		result.delete(result.length() - 2, result.length());
+
+		return result.toString();
 	}
 }
