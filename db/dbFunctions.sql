095184dffbf0052a98ae5c8c50384a8ca87c77d8
simon.stemper@gmail.com <simon.stemper@gmail.com>
2015-07-12 00:09:54 +0200

    Updated some sql scripts

--- db/dbFunctions.sql
+++ db/dbFunctions.sql
@@ -3,14 +3,14 @@ CREATE FUNCTION HYLLEBLOMST.GROUP_CONCAT
     RETURNS VARCHAR(2000)
     PARAMETER STYLE DERBY
     NO SQL LANGUAGE JAVA
-	EXTERNAL NAME 'GroupConcat.groupConcat'
+	EXTERNAL NAME 'de.uniba.kinf.projm.hylleblomst.database.utils.GroupConcat.groupConcat';
 	
-DROP function HYLLEBLOMST.GROUP_CONCAT	
+DROP function HYLLEBLOMST.GROUP_CONCAT;
 
 SELECT HYLLEBLOMST.GROUP_CONCAT (', ',QuellenName,FakultaetenNorm) FROM HYLLEBLOMST.quellen, HYLLEBLOMST.fakultaeten WHERE Hylleblomst.quellen.quellenID<=3 AND Hylleblomst.fakultaeten.fakultaetenID<=3; 
 SELECT HYLLEBLOMST.GROUP_CONCAT (',',Adlig,Jesuit) FROM HYLLEBLOMST.PERSON WHERE PersonID<10;
 
 CALL SQLJ.INSTALL_JAR (
-	'groupConcat.jar','HYLLEBLOMST.groupConcat',0)
+	'C:\Users\Simon\git\KInf-Projekt\lib\groupconcat.jar','HYLLEBLOMST.groupconcat',0);
 CALL SQLJ.REMOVE_JAR (
-	'HYLLEBLOMST.groupConcat.jar',0)
+	'HYLLEBLOMST.groupconcat.jar',0)
