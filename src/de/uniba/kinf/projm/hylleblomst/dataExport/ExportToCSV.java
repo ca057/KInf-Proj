759c5c449f138b0d028fc275fd5846b3cecd43a7
simon.stemper@gmail.com <simon.stemper@gmail.com>
2015-07-11 11:21:14 +0200

    Updated export to csv

--- src/de/uniba/kinf/projm/hylleblomst/dataExport/ExportToCSV.java
+++ src/de/uniba/kinf/projm/hylleblomst/dataExport/ExportToCSV.java
@@ -2,6 +2,8 @@ package de.uniba.kinf.projm.hylleblomst.dataExport;
 
 import java.io.File;
 
+import javax.sql.rowset.CachedRowSet;
+
 import de.uniba.kinf.projm.hylleblomst.exceptions.ExportException;
 
 public class ExportToCSV {
@@ -10,8 +12,12 @@ public class ExportToCSV {
 		// TODO Auto-generated constructor stub
 	}
 
-	public void run(File file) throws ExportException {
+	public void run(File file, CachedRowSet crs) throws ExportException {
+		if (file != null) {
 
+		} else {
+			throw new ExportException("No suitable file to save to");
+		}
 	}
 
 }
