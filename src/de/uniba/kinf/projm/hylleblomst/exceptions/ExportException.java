177004bd342a8a0acb078a2ac77d1b3d055ca0c4
simon.stemper@gmail.com <simon.stemper@gmail.com>
2015-07-11 17:50:34 +0200

    Javadoc for exceptions

--- src/de/uniba/kinf/projm/hylleblomst/exceptions/ExportException.java
+++ src/de/uniba/kinf/projm/hylleblomst/exceptions/ExportException.java
@@ -1,29 +1,33 @@
 package de.uniba.kinf.projm.hylleblomst.exceptions;
 
+/**
+ * This exception is thrown if an error occurs while exporting data.
+ * 
+ * @author Simon
+ *
+ */
 public class ExportException extends Exception {
 
+	private static final long serialVersionUID = -625283672497630065L;
+
 	public ExportException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public ExportException(String arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 	public ExportException(Throwable arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 	public ExportException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
 	}
 
-	public ExportException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+	public ExportException(String arg0, Throwable arg1, boolean arg2,
+			boolean arg3) {
 		super(arg0, arg1, arg2, arg3);
-		// TODO Auto-generated constructor stub
 	}
 
 }
--- src/de/uniba/kinf/projm/hylleblomst/exceptions/ViewException.java
+++ src/de/uniba/kinf/projm/hylleblomst/exceptions/ViewException.java
@@ -1,12 +1,21 @@
 package de.uniba.kinf.projm.hylleblomst.exceptions;
 
+/**
+ * This exception is thrown if an error occurs during the view of the program.
+ * 
+ * @author Simon
+ *
+ */
 public class ViewException extends Exception {
+
+	private static final long serialVersionUID = -6395504747881967115L;
 
 	public ViewException() {
 		super();
 	}
 
-	public ViewException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+	public ViewException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
 	}
 
